d6c96e4d42f74a11ce42391fa323bbbd9ca384b1
JoaoVictor1011 <joaovictorcandido16@gmail.com>
2023-03-24 14:15:49 -0300

    add header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,35 +1,37 @@
-import * as react from 'react';
-import { Nav } from 'react-bootstrap';
+import * as react from "react";
+import { Nav } from "react-bootstrap";
 
 /* folha css */
-import '../Styles/header.css';
+import "../Styles/header.css";
 
-
-import Logo from '../Assets/Logo1.png'
-import Carrinho from '../Assets/carrinho.png';
+import Logo from "../Assets/Logo1.png";
+import Carrinho from "../Assets/carrinho.png";
 
 const Header = () => {
-    return(
-        <div className='headerstyle'>
-            <Nav className='justify-content-center'>
-                <img className='logostyle'src={Logo} alt='logotipo'/>
-                <Nav.Link className='button' href='#'> Bebidas </Nav.Link>
-                <Nav.Link className='button' href='#'> Caixas </Nav.Link>
-                <Nav.Link className='button' href='#'> Sobre </Nav.Link>
-                <Nav.Link className='button' href='#'> Contato </Nav.Link>
-                <img className='cartstyle'src={Carrinho} alt='carrinho de compras'/>
-            </Nav>
-        </div>
-    );
-}
+  return (
+    <div className="headerstyle">
+      <Nav className="justify-content-center">
+        <img className="logostyle" src={Logo} alt="logotipo" />
+        <Nav.Link className="button" href="./bebidas">
+          {" "}
+          Bebidas{" "}
+        </Nav.Link>
+        <Nav.Link className="button" href="./caixas">
+          {" "}
+          Caixas{" "}
+        </Nav.Link>
+        <Nav.Link className="button" href="#">
+          {" "}
+          Sobre{" "}
+        </Nav.Link>
+        <Nav.Link className="button" href="#">
+          {" "}
+          Contato{" "}
+        </Nav.Link>
+        <img className="cartstyle" src={Carrinho} alt="carrinho de compras" />
+      </Nav>
+    </div>
+  );
+};
 
 export default Header;
-
-
-
-
-
-
-
-
-
