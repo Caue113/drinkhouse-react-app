ba3a397ff00783548f088213d4d107ae3b219fb0
Luis Felipe T. Saturnino <luisftsat@gmail.com>
2023-03-28 16:28:55 -0300

    Temp Feature - ongoing production of automatic bebida page generator

--- src/Pages/bebida.jsx
+++ src/Pages/bebida.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Nav } from "react-bootstrap";
+import { Button, Carousel, Nav } from "react-bootstrap";
 import { json, Link, Outlet, useLoaderData, useParams } from "react-router-dom";
 import "./bebida.css";
+import Header from "../Layout/Header.jsx";
+import Footer from "../Layout/Footer.jsx";
 
 
 export async function loader({ params }){
     console.log("Rodando PARAMS de LOADER");
     console.log(params);
 
-    let result;
+    let SQL_RESULT;
 
     try {
         await axios.get(`http://localhost:3002/bebidas/bebida/${params.bebidaId}`)
                 .then((res) =>{
-                    result = res.data;
+                    SQL_RESULT = res.data;
                 });
         
-        return {result};
+        return SQL_RESULT;
     } catch (error) {
         console.warn(error);
         throw error;
@@ -26,37 +28,116 @@ export async function loader({ params }){
 }
 
 
-function Bebida (){
-    const bebidas = useLoaderData();
+function promocaoElement(){
 
-    const renderBebidasRow = () =>{
-        let temp = [];
-        bebidas.result.forEach((value)=>{
-            console.log(value)
-            temp.push(<p key={value.bebida_id}>{value.bebida_id} {value.nome}</p>);
-        });
+    let hasPromocao;
 
-        return (temp);
+    if(hasPromocao){
+        return(
+            <p> Promoção de 20% </p>
+        );
     }
 
     
+}
+
+function buttonState(){
+    let button;
+    let hasInStock = true;  //Pegar valor da database
+
+    if(hasInStock){
+        button = <Button>Comprar</Button>
+    }
+    else{
+        button = <Button disabled={true}>Fora de Estoque</Button>
+    }
+    return(button);
+}
+
+
+function Bebida (){
+    const SQL_DATA = useLoaderData();
+    console.log(SQL_DATA)
+    console.log(SQL_DATA[0].nome)
+
+
+    //Lógica temporária. Deve ser feita em outro ambiente
+    if(SQL_DATA === undefined || SQL_DATA.length === 0){
+        return(
+            <>
+                <h1>Bebida não encontrada</h1>
+                <p>Lamentamos, mas parece que essa bebida não existe :(</p>
+            </>
+        );
+    }
+    
     return(
-        <div className="container">
-            <Nav className="nav">
-                <Nav.Item><Link to={"/"} className="nav__link">Menu Principal</Link></Nav.Item>
-                <Nav.Item><Link to={"/bebidas"} className="nav__link">Menu de bebidas</Link></Nav.Item>
-            </Nav>
+        <>
+            <Header></Header>
+            <main className="container">
 
-            <div className="row">
-                <img src="#" alt="bebida"></img>
-                <div>{renderBebidasRow()}</div>
-                
-                <p>Bebida supimpa:</p>
-            </div>
+                <div className="row">
+                    <h1>{SQL_DATA[0].nome}</h1>
+                </div>
 
-            <div className="row">
-            </div>
-        </div>
+                <div className="row" style={{backgroundColor: "orange"}}>
+                    <div className="d-flex flex-row">
+                        <div>
+                            <img src="https://placekitten.com/g/200/300" class="img-fluid" alt="bebida"></img>
+                        </div>
+
+                        <div className="d-flex flex-column">
+                            <h2 className="text-center">{SQL_DATA[0].nome} - Marca X</h2>
+                            {promocaoElement()}
+                            
+                            <div className="d-flex flex-row">
+                                <div className="col-4">
+                                    <p>Preço: R$ {"preco"}</p>
+                                    
+                                </div>
+                                <div className="col-4">
+                                    <p>Volume: R$ {"volume"}</p>
+                                </div>
+                            </div>
+
+                            <div className="d-flex flex-row">
+                                <div className="col-12">
+                                    <p>Porcentagem Alcoolica</p>
+                                    <div>
+                                        {"QuantidadeGotasAlcoolico"}
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="d-flex flex-row">
+                                <div className="col-12">
+                                    <p>Porcentagem Alcoolica</p>
+                                    <div>
+                                        {"QuantidadeGotasAlcoolicoComponente"}
+                                    </div>
+                                </div>
+                            </div>
+                            {buttonState()}
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="row">
+                    <h2>Bebidas Relacionadas</h2>
+
+                    <div className="d-flex flex-direction-row">
+                        <img src="https://placekitten.com/g/100/100" className=""></img>
+                        <img src="https://placekitten.com/g/100/100" className=""></img>
+                        <img src="https://placekitten.com/g/100/100" className=""></img>
+                        <img src="https://placekitten.com/g/100/100" className=""></img>
+                    </div>
+
+                </div>
+            </main>
+            <Footer></Footer>
+        </>
+        
     );
 }
 
