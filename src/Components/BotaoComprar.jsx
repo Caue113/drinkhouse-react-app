5dcad6509c1671340cd2dde993d3b9f68a119432
Louis <luizfernandodedelta2015@gmail.com>
2023-04-10 14:46:27 -0300

    path correction

--- src/Components/BotaoComprar.jsx
+++ src/Components/BotaoComprar.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-function addProdutoCarrinhoSession(id){
-    //Backend - Adaptar para usar sistema de SESSION
-    //Código para adicionar no carrinho.
-    //sessionStorage.setItem("produtoId",id);
-    console.log("Adicionado produto!");
+function addProdutoCarrinhoSession(id) {
+  //Backend - Adaptar para usar sistema de SESSION
+  //Código para adicionar no carrinho.
+  //sessionStorage.setItem("produtoId",id);
+  console.log("Adicionado produto!");
 }
 
-function BotaoComprar(props){
-    let button;
+function BotaoComprar(props) {
+  let button;
 
-    if(props.estoque === 0 || props.estoque === null){
-        button = <Button disabled={true}>Fora de Estoque</Button>
-    }
-    else{
-        button = <Button onClick={()=>{addProdutoCarrinhoSession(props.id)}}>Comprar</Button>
-    }
-
-    return(
-        <div>
-            {button}
-        </div>
+  if (props.estoque === 0 || props.estoque === null) {
+    button = <Button disabled={true}>Fora de Estoque</Button>;
+  } else {
+    button = (
+      <Button
+        onClick={() => {
+          addProdutoCarrinhoSession(props.id);
+        }}
+      >
+        Comprar
+      </Button>
     );
+  }
+
+  return <div>{button}</div>;
 }
 
 export default BotaoComprar;
